a37df38fae44af5841bfe4f3d10dc6b20126e425
Jichao Luo <2112106182@e.gzhu.edu.cn>
2023-09-15 15:40:57 +0800

    data_convert.py: update the data_convert function. README.md: update readme

--- README.md
+++ README.md
@@ -4,34 +4,52 @@
 
 ## Environments and Installation
 
+The basic language for our work is [python](https://www.python.org/), and the baseline
+is [nnU-Net](https://github.com/MIC-DKFZ/nnUNet/tree/nnunetv1). So, you can install the nnunet frame with
+the [GitHub Repository](https://github.com/MIC-DKFZ/nnUNet/tree/nnunetv1), or use the following comments:
+
+```
+pip install torch torchvision torchaudio
+pip install -e .
+```
+
 ## Usage
 
 ### convert CT images to npy
 
-The baseline module is nnU-Net, and it must convert data to npy files follow
-code [nnU-Net](https://github.com/MIC-DKFZ/nnUNet/tree/nnunetv1).
+we modify the normalization function with ___preprocessing.py___,
+and you could use the following comments to processing the CT images:
+
+```
+python nnunet/experiment_planning/nnUNet_convert_decathlon_task.py -i [FLARE23_imageTr_path]
+
+python nnunet/experiment_planning/nnUNet_plan_and_preprocess -t [TASK_ID] --verify_dataset_integrity
+```
+
+It must be noted that the method is based on the __nnU-Net__, so I recommend you to convert the dataset within nnU-Net's
+data preprocessing.
+
+The usage and note concerning for ___nnUNet_convert_decathlon_task.py___ is recorded
+on [website](https://github.com/MIC-DKFZ/nnUNet/blob/nnunetv1/documentation/dataset_conversion.md).
 
 After preprocessing, we will obtain several folders:
-```
--- nnU-Net_base_folder
-    -- nnUNet_prepocessing
-    -- nnUNetFrame
-    -- nnUNet_raw
-    -- nnUNet_trained_models
-```
-
-### generate and process pseudo labels
-
-### update cases info
-
-For Simplifying the processes, you can use the following code to convert the dataset:
 
 ```
-python data_convert.py -PSEUDO_LABEL_PATH --NNUNET_npy_PATH
+- nnU-Net_base_folder
+    - nnUNet_prepocessing
+    - nnUNetFrame
+    - nnUNet_raw
+    - nnUNet_trained_models
 ```
 
-It must be noted that the method is based on the nnU-Net, so I recommend you to convert the dataset within nnU-Net's
-data preprocessing.
+### generate and process pseudo labels and update dataset
+
+To Simplifying the processes, you can use the following code to convert the dataset:
+
+```
+python data_convert.py -pseudo_label_folder -nnunet_preprocessing_folder -imagesTr_floder -labelTr_floder
+```
+where the __nnunet_preprocessing_folder__ is the folder path of the dataset planed by nnunet. like 'nnU-Net_base_folder/nnUNet_preprocessed/Task098_FLARE2023/nnUNetData_plans_v2.1_stage1'
 
 ## Training
 
@@ -40,3 +58,11 @@ python nnunet/run/run_training.py 3d_fullres nnUNetTrainerV2 DATA_ID FOLD[0,1,2,
 ```
 
 ## Inference
+```
+python inference.py [INPUT_FOLDER] [OUTPUT_FOLDER]
+```
+Before the Inference, you should move the best nnunet checkpoints to replace the three files in folder __'checkpoints'__.
+
+## What's New?
+
+we have fix the problem the influence from data dtype which may effects the final result!
--- data_convert.py
+++ data_convert.py
@@ -6,6 +6,19 @@ import pickle
 from skimage.transform import resize
 
 
+def get_bbox_from_mask_b(nonzero_mask, outside_value):
+    mask_voxel_coords = np.where(nonzero_mask != outside_value)
+    minzidx = int(np.min(mask_voxel_coords[0]))
+    maxzidx = int(np.max(mask_voxel_coords[0])) + 1
+    minxidx = int(np.min(mask_voxel_coords[1]))
+    maxxidx = int(np.max(mask_voxel_coords[1])) + 1
+    minyidx = int(np.min(mask_voxel_coords[2]))
+    maxyidx = int(np.max(mask_voxel_coords[2])) + 1
+    bbox = [[minzidx, maxzidx], [minxidx, maxxidx], [minyidx, maxyidx]]
+
+    return bbox
+
+
 def get_all_file_paths(folder_path, P='.npz'):
     file_paths = []
     # 遍历文件夹中的所有文件和文件夹
@@ -26,7 +39,27 @@ def save_pickle(obj, path):
     pickle.dump(obj, open(path, 'wb+'))
 
 
-def convert(pseudo_paths, nnunet_npy_paths):
+def resample_data(ori_array, ori_spacing,
+                  target_spacing=None, only_z=False):
+    # shape c w h d
+    # spacing_nnunet = [1.8532123022052305, 1.512973664256994, 1.512973664256994]
+    if target_spacing is None:
+        target_spacing = [2.2838839, 1.8709113, 1.8709113]
+    if only_z:
+        target_spacing = [target_spacing[0], ori_spacing[0], ori_spacing[1]]
+    ori_shape = ori_array.shape[1:]
+    target_shape = [ori_spacing[i] * ori_shape[i] / target_spacing[i] // 1 for i in
+                    range(len(ori_shape))]
+    reshaped_data = []
+    reshaped_data.append(resize(ori_array[0], target_shape, order=3)[None])
+    reshaped_data.append(resize(ori_array[1], target_shape, order=0, preserve_range=True,
+                                anti_aliasing=False)[None])
+    reshaped_data.append(resize(ori_array[-1], target_shape, order=0, preserve_range=True,
+                                anti_aliasing=False)[None])
+    return np.vstack(reshaped_data), target_spacing
+
+
+def convert(pseudo_paths, nnunet_npy_paths, CT_paths, label_paths):
     '''
 
     :param pseudo_paths: the path of pseudo labels, all the file must be ending with '*.nii.gz'.
@@ -36,32 +69,63 @@ def convert(pseudo_paths, nnunet_npy_paths):
     # 1. input data convert to npy
     #       converted by nnunet already
     # 2. pseudo label convert into npy
-    pseudo_path_list = get_all_file_paths(pseudo_paths)
+    pseudo_path_list = get_all_file_paths(pseudo_paths, '.nii.gz')
+    CT_path_list = get_all_file_paths(CT_paths, '.nii.gz')
+    label_path_list = get_all_file_paths(label_paths, '.nii.gz')
     npy_path_list = get_all_file_paths(nnunet_npy_paths)
-    assert len(pseudo_path_list) == len(npy_path_list), 'length for pseudo labels must be same as nii files'
-    pseudo_path_list.sort()
-    npy_path_list.sort()
-    for p_path, npy_path in zip(pseudo_paths, nnunet_npy_paths):
+    assert len(pseudo_path_list) == len(npy_path_list) == len(npy_path_list) == len(
+        label_path_list), 'length for pseudo labels must be same as nii files'
+    for p_path, npy_path, CT_path, label_path in zip(pseudo_path_list, npy_path_list, CT_path_list, label_path_list):
         # load npy
         if not os.path.exists(npy_path.replace('.npz', '.npy')):
             images = np.load(npy_path, allow_pickle=True)
         else:
             images = np.load(npy_path.replace('.npz', '.npy'), allow_pickle=True)
+
         # load nii
         pseudo_array = sitk.GetArrayFromImage(sitk.ReadImage(p_path)).astype(np.float32)
+        CT_array = sitk.GetArrayFromImage(sitk.ReadImage(CT_path)).astype(np.float32)
+        label_array = sitk.GetArrayFromImage(sitk.ReadImage(label_path)).astype(np.float32)
+
         # load info
         properties = load_pickle(npy_path.replace('.npz', '.pkl'))
-        # crop array
-        crop_bbox = properties['crop_bbox']
-        pseudo_array = pseudo_array[slice(crop_bbox)]
-        # resample pseudo_array
-        size_after_resampling = properties['size_after_resampling']
-        pseudo_array = resize(pseudo_array, size_after_resampling, order=3, preserve_range=True,
-                              anti_aliasing=False)
+
+        # update the crop bbox
+        crop_bbox = get_bbox_from_mask_b((pseudo_array + label_array) > 0, outside_value=0)
+        sli = slice(crop_bbox[0][0], crop_bbox[0][1]), slice(crop_bbox[1][0], crop_bbox[1][1]), slice(crop_bbox[2][0],
+                                                                                                      crop_bbox[2][1])
+        # crop three arrays
+        properties['crop_bbox'] = crop_bbox
+        pseudo_array = pseudo_array[sli]
+        CT_array = CT_array[sli]
+        label_array = label_array[sli]
+        properties['size_after_cropping'] = np.array(pseudo_array.shape)
+
         # concate
-        all_data = np.concatenate((images[0], images[1], pseudo_array), axis=0)
+        cropped_data = np.stack((CT_array, label_array, pseudo_array), axis=0)
+
+        # resample array
+        resampled_data, current_spacing = resample_data(cropped_data, properties['original_spacing'],
+                                                        properties['spacing_after_resampling'])
+
+        # norm one
+        ct_array = resampled_data[0].copy()
+        if np.max(ct_array) < 1:
+            percentile_95 = np.percentile(ct_array, 95)
+            percentile_5 = np.percentile(ct_array, 5)
+            std = np.std(ct_array)
+            mn = np.mean(ct_array)
+            ct_array = np.clip(ct_array, a_min=percentile_5, a_max=percentile_95).astype(np.float32)
+            ct_array = (ct_array - mn) / std
+        else:
+            ct_array = np.clip(ct_array, a_min=-160., a_max=240.).astype(np.float32)
+            ct_array = (ct_array + 160.) / 400.
+        resampled_data[0] = ct_array
+
+        properties['size_after_resampling'] = np.array(resampled_data[0].shape)
+
         # save to npy
-        np.save(npy_path.replace('.npz', '.npy'), images)
+        np.save(npy_path.replace('.npz', '.npy'), resampled_data)
         print('finish combine')
 
         # 3. location info add into pkl
@@ -73,7 +137,7 @@ def convert(pseudo_paths, nnunet_npy_paths):
         for c in all_classes:
             if c == 0:
                 continue
-            all_locs = np.argwhere(all_data[-1] == c or all_data[-2] == c)
+            all_locs = np.argwhere(resampled_data[-2 if c in resampled_data[-1] else -1] == c)
             if len(all_locs) == 0:
                 class_locs[c] = []
                 continue
@@ -87,14 +151,16 @@ def convert(pseudo_paths, nnunet_npy_paths):
         save_pickle(properties, npy_path.replace('.npz', '.pkl'))
 
 
-
-
 if __name__ == '__main__':
     argps = argparse.ArgumentParser()
     # -PSEUDO_LABEL_PATH --NNUNET_npy_PATH
-    argps.add_argument('PSEUDO_LABEL_PATH')
-    argps.add_argument('NNUNET_npy_PATH')
+    argps.add_argument('pseudo_path', type=str, help='path for pseudo label, all file must end with .nii.gz!')
+    argps.add_argument('nnunet_npy_path', type=str,
+                       help='path for npy from nnunet, it must be convert by nnunet first!')
+    argps.add_argument('image_tr_path', type=str, help='path for npy from FLARE imageTr')
+    argps.add_argument('label_tr_path', type=str, help='path for npy from FLARE labelTr')
 
     arg_s = argps.parse_args()
 
-    convert(pseudo_paths=arg_s['PSEUDO_LABEL_PATH'], nnunet_npy_paths=arg_s['NNUNET_npy_PATH'])
+    convert(pseudo_paths=arg_s.pseudo_path, nnunet_npy_paths=arg_s.nnunet_npy_path, CT_paths=arg_s.image_tr_path,
+            label_paths=arg_s.label_tr_path)
